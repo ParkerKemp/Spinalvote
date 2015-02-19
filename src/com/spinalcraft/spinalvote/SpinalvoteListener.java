3e38b29fa1a05a415411d06bb1d4b6adadc83f32
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-02-19 02:01:57 -0600

    Deleted commented-out code

--- src/com/spinalcraft/spinalvote/SpinalvoteListener.java
+++ src/com/spinalcraft/spinalvote/SpinalvoteListener.java
@@ -54,46 +54,6 @@ public class SpinalvoteListener implements Listener{
 	public void voteReward(Player player){
 		PlayerInventory inventory = player.getInventory();
 		inventory.addItem(new ItemStack(Material.EXP_BOTTLE, 24));
-		//target.setLevel(target.getLevel() + 15);
 		player.sendMessage(Spinalpack.code(Co.BLUE) + "You got 24 exp bottles for voting!");
 	}
 }
-/*public static void insertVoteRecord(String username, String timestamp, String service){
-String query;
-query = "INSERT INTO Votes(username, date, service) values('" + username + "', '" + timestamp + "', '" + service + "')";
-try {
-	Statement stmt = conn.createStatement();
-	stmt.executeUpdate(query);
-} catch (SQLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-}
-
-private boolean unlinkSignWithUuid(String uuid, int slipno) {
-		String query;
-		PreparedStatement stmt;
-		query = "UPDATE Slips SET w" + slipno + " = NULL WHERE uuid = ?";
-		try {
-			stmt = Spinalpack.prepareStatement(query);
-			stmt.setString(1, uuid);
-			stmt.executeUpdate();
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-public static void createVoteTable(){
-		String query;
-		try {
-			query = "CREATE TABLE IF NOT EXISTS Votes (ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(31), date VARCHAR(63), service VARCHAR(63))";
-			Statement stmt = conn.createStatement();
-			stmt.executeUpdate(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-*/
